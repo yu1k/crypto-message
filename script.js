fe6b8bd222c7f4e2fbce422b9e32b86c6d969aa1
yuik <48705423+yu1k@users.noreply.github.com>
2020-02-03 03:24:06 +0900

    スクリプトのコードが書けたので更新

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-var a = 'b'.charCodeAt(0); 
-for(var i = 0; i < a.length; i++){
-    a++;
+function message(){
+    var input_message = document.getElementById("input_message").value;
+    var input_message = input_message.charCodeAt(0); 
+    for(var i = 0; i < 1; i++){
+        input_message++;
+    }
+    input_message = "暗号化した結果は「" + String.fromCharCode(input_message) + "」です。";
+    document.getElementById("output_message").innerHTML = input_message;
+    /***  テスト用  ***/
+    /*
+    var a = 'a'.charCodeAt(0); 
+    for(var i = 0; i < 1; i++){
+        a++;
+    }
+    console.log(String.fromCharCode(a));*/
 }
-console.log(String.fromCharCode(a));
+
+function main() {
+    message();
+}
